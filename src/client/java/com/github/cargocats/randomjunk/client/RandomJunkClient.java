b3611887033f70b75b47b3f51217db91f180937e
cargocats <ceggy@protonmail.com>
2025-05-11 18:21:20 -0400

    Lidocaine tooltip

--- src/client/java/com/github/cargocats/randomjunk/client/RandomJunkClient.java
+++ src/client/java/com/github/cargocats/randomjunk/client/RandomJunkClient.java
@@ -3,7 +3,11 @@ package com.github.cargocats.randomjunk.client;
 import com.github.cargocats.randomjunk.RandomJunk;
 import com.github.cargocats.randomjunk.client.registry.RJEntityModelLayers;
 import com.github.cargocats.randomjunk.client.registry.RJEntityRenderers;
+import com.github.cargocats.randomjunk.registry.RJItems;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 
 public class RandomJunkClient implements ClientModInitializer {
 
@@ -11,7 +15,17 @@ public class RandomJunkClient implements ClientModInitializer {
     public void onInitializeClient() {
         RJEntityModelLayers.initialize();
         RJEntityRenderers.initialize();
+        registerItemTooltips();
 
         RandomJunk.LOG.info("Initialized Random Junk client!");
+    }
+
+    private static void registerItemTooltips() {
+        ItemTooltipCallback.EVENT.register((stack, tooltipContext, tooltipType, textList) -> {
+            if (stack.isOf(RJItems.LIDOCAINE)) {
+                textList.add(Text.literal("Extinguishes and gives regeneration if on fire.").formatted(Formatting.GRAY));
+                textList.add(Text.literal("Use too much and you might overdose!").formatted(Formatting.RED));
+            }
+        });
     }
 }
