4a6e9570462b56723c383c651813c25700eba8ec
cargocats <ceggy@protonmail.com>
2025-05-15 17:07:56 -0400

    Stop milk from affecting Overdose Effect

---
+++ src/main/java/com/github/cargocats/randomjunk/mixin/ClearAllEffectsConsumeEffectMixin.java
@@ -0,0 +1,34 @@
+package com.github.cargocats.randomjunk.mixin;
+
+import com.github.cargocats.randomjunk.RandomJunk;
+import com.github.cargocats.randomjunk.registry.RJStatusEffects;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.consume.ClearAllEffectsConsumeEffect;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(ClearAllEffectsConsumeEffect.class)
+public class ClearAllEffectsConsumeEffectMixin {
+    @WrapOperation(method = "onConsume", at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/entity/LivingEntity;clearStatusEffects()Z"
+    ))
+
+    private boolean preserveOverdoseEffect(LivingEntity user, Operation<Boolean> original, World world, ItemStack stack) {
+        RandomJunk.LOG.info("The entity in question: {}, item stack: {}", user, stack);
+
+        StatusEffectInstance effect = user.getStatusEffect(RJStatusEffects.OVERDOSE);
+        boolean cleared = original.call(user);
+
+        if (effect != null) {
+            user.addStatusEffect(effect);
+        }
+
+        return cleared;
+    }
+}
