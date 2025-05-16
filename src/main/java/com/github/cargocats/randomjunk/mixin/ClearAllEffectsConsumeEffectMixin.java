36e3b1fd2d1064c3244b0b32d79b624515a0ae24
cargocats <ceggy@protonmail.com>
2025-05-15 21:28:27 -0400

    Don't keep overdose if used totem

--- src/main/java/com/github/cargocats/randomjunk/mixin/ClearAllEffectsConsumeEffectMixin.java
+++ src/main/java/com/github/cargocats/randomjunk/mixin/ClearAllEffectsConsumeEffectMixin.java
@@ -7,6 +7,7 @@ import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.item.consume.ClearAllEffectsConsumeEffect;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
@@ -25,7 +26,7 @@ public class ClearAllEffectsConsumeEffectMixin {
         StatusEffectInstance effect = user.getStatusEffect(RJStatusEffects.OVERDOSE);
         boolean cleared = original.call(user);
 
-        if (effect != null) {
+        if (effect != null && !stack.isOf(Items.TOTEM_OF_UNDYING)) {
             user.addStatusEffect(effect);
         }
 
