b087ea5b2737ed81a8061f06b6cb3aa0db61905f
cargocats <ceggy@protonmail.com>
2025-06-04 19:29:34 -0400

    Remove punctuation from log

--- src/client/java/com/github/cargocats/randomjunk/client/datagen/RJDynamicRegGenerator.java
+++ src/client/java/com/github/cargocats/randomjunk/client/datagen/RJDynamicRegGenerator.java
@@ -1,10 +1,8 @@
 package com.github.cargocats.randomjunk.client.datagen;
 
-import com.github.cargocats.randomjunk.registry.RJDamageTypes;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricDynamicRegistryProvider;
-import net.minecraft.entity.damage.DamageScaling;
-import net.minecraft.entity.damage.DamageType;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.RegistryWrapper;
 
 import java.util.concurrent.CompletableFuture;
@@ -16,11 +14,7 @@ public class RJDynamicRegGenerator extends FabricDynamicRegistryProvider {
 
     @Override
     protected void configure(RegistryWrapper.WrapperLookup wrapperLookup, Entries entries) {
-        entries.add(RJDamageTypes.OVERDOSE, new DamageType(
-                "overdose",
-                DamageScaling.WHEN_CAUSED_BY_LIVING_NON_PLAYER,
-                1.0f
-        ));
+        entries.addAll(wrapperLookup.getOrThrow(RegistryKeys.DAMAGE_TYPE));
     }
 
     @Override
