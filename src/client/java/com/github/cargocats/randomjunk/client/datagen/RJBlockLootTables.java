fae33e65f055d7baa112eeabcb9ab04c1a59df77
cargocats <ceggy@protonmail.com>
2025-06-18 18:42:26 -0400

    Block Loot Tables support

---
+++ src/client/java/com/github/cargocats/randomjunk/client/datagen/RJBlockLootTables.java
@@ -0,0 +1,20 @@
+package com.github.cargocats.randomjunk.client.datagen;
+
+import com.github.cargocats.randomjunk.registry.RJBlocks;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
+import net.minecraft.registry.RegistryWrapper;
+
+import java.util.concurrent.CompletableFuture;
+
+public class RJBlockLootTables extends FabricBlockLootTableProvider {
+    public RJBlockLootTables(FabricDataOutput dataOutput, CompletableFuture<RegistryWrapper.WrapperLookup> registryLookup) {
+        super(dataOutput, registryLookup);
+    }
+
+    @Override
+    public void generate() {
+        addDrop(RJBlocks.SAFE_BLOCK);
+        addDrop(RJBlocks.TOILET_PAPER);
+    }
+}
