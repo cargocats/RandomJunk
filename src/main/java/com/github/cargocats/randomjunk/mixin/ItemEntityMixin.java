7c0be40436f06616445b46249868f71932984f7b
cargocats <ceggy@protonmail.com>
2025-07-09 19:30:01 -0400

    Fix pipe bomb crash about ClientWorld unable to be casted to ServerWorld

--- src/main/java/com/github/cargocats/randomjunk/mixin/ItemEntityMixin.java
+++ src/main/java/com/github/cargocats/randomjunk/mixin/ItemEntityMixin.java
@@ -1,14 +1,11 @@
 package com.github.cargocats.randomjunk.mixin;
 
 import com.github.cargocats.randomjunk.registry.RJItems;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.world.ServerWorld;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -16,18 +13,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Debug(export = true)
 @Mixin(ItemEntity.class)
 public abstract class ItemEntityMixin {
-    @Shadow public abstract ItemStack getStack();
-
-    @Shadow
-    public abstract @Nullable Entity getOwner();
-
     @Inject(method = "tick", at = @At("TAIL"))
     private void onTick(CallbackInfo ci) {
-        ItemStack itemStack = this.getStack();
-        Entity entity = this.getOwner();
+        ItemEntity itemEntity = (ItemEntity) (Object) this;
+        ItemStack itemStack = itemEntity.getStack();
 
-        if (itemStack.isOf(RJItems.PIPE_BOMB) && entity != null && !entity.getWorld().isClient) {
-            itemStack.getItem().inventoryTick(itemStack, (ServerWorld) entity.getWorld(), entity, null);
+        if (itemStack.isOf(RJItems.PIPE_BOMB) && !itemEntity.getWorld().isClient) {
+            itemStack.getItem().inventoryTick(itemStack, (ServerWorld) itemEntity.getWorld(), itemEntity, null);
         }
     }
 }
